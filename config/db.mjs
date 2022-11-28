f1f8a95e926b6c029abd041efb00e53a546deeb3
kifeb <rizkyfebrianto947@gmail.com>
2022-11-28 17:46:21 +0700

    make it better

--- config/db.mjs
+++ config/db.mjs
@@ -1,16 +1,16 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2";
 
-const db = new Sequelize("social_api", "root", "Kifeb99##", {
-  host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql"
 })
 
 export const database = mysql.createConnection({
-  database: "social_api",
-  user: "root",
-  password: "Kifeb99##",
-  host: "localhost"
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST
 })
 
 export default db;
